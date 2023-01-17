fc1d073bf7b69552297bfb9f3176583f6d7372e9
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-17 12:14:12 -0500

    built out the basic structure of the Todo list

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,21 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>Todo App</h1>
+        <h2>todos:</h2>
+
+        <ul>
+          <li>Walk the dog</li>
+          <li>Learn React</li>
+          <li>Havefun</li>
+        </ul>
+
+        <form>
+          <input/>
+          <button>Add</button>
+        </form>
+
+        <button>Clear</button>
       </div>
     )
   }
