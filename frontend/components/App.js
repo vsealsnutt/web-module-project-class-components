1f1008a329ddacc6dd65760a44a4d415bc346d62
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-17 12:52:41 -0500

    built Form component

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoList from './TodoList';
+import Form from './Form';
 
 export default class App extends React.Component {
   constructor() {
@@ -34,10 +35,7 @@ export default class App extends React.Component {
 
         <TodoList todos={todos} />
 
-        <form>
-          <input/>
-          <button>Add</button>
-        </form>
+        <Form />
 
         <button>Clear</button>
       </div>
