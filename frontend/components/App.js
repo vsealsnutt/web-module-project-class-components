2baa89b7949398ec5a96438c973201dc3f1d3cc2
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-17 12:32:06 -0500

    used state to build out the list

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,16 +1,42 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Walk the dog',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          name: 'Learn React',
+          id: 1528817084358,
+          completed: false
+        },
+        {
+          name: 'Have fun',
+          id: 1528817098522,
+          completed: false
+        }
+      ]
+    }
+  }
+
   render() {
+    const { todos } = this.state;
+
     return (
       <div>
-        <h1>Todo App</h1>
-        <h2>todos:</h2>
+        <h1>Todos:</h1>
 
         <ul>
-          <li>Walk the dog</li>
-          <li>Learn React</li>
-          <li>Havefun</li>
+          {
+            todos.map(todo => {
+              return (<li>{todo.name} { todo.completed ? <span>- completed</span> : <span></span> }</li>)
+            })
+          }
         </ul>
 
         <form>
