c56dab9559c7b336bc528d0630d8000cf0608297
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-18 09:59:25 -0500

    Add button is fully functional. User can type in the input box and add a task

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,15 +1,29 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
   handleSubmit = (event) => {
     event.preventDefault();
-    this.props.handleAdd();
+    this.props.handleAdd(this.state.input);
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      ...this.state,
+      input: event.target.value
+    });
   }
 
   render() {
     return (
       <form>
-        <input/>
+        <input onChange={this.handleChange} />
         <button onClick={this.handleSubmit}>Add</button>
       </form>
     )
